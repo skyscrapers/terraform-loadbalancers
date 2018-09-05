d71a647bcb462cc89a0302823927c58af2b10d8d
Luca Venturelli <luca@skyscrapers.eu>
2018-09-05 12:38:05 +0200

    changing sg name

--- alb/sg.tf
+++ alb/sg.tf
@@ -1,10 +1,10 @@
 resource "aws_security_group" "sg_alb" {
-  name_prefix = "${var.name_prefix}"
+  name        = "${var.project}-${var.environment}-${var.name_prefix}-sg-alb"
   description = "Security group for ALB ${var.project}-${var.environment}-${var.name_prefix}-alb"
   vpc_id      = "${var.vpc_id}"
 
   tags = "${merge("${var.tags}",
-    map("Name", "${var.project}-${var.environment}-${var.name_prefix}-sg_alb",
+    map("Name", "${var.project}-${var.environment}-${var.name_prefix}-sg-alb",
       "Environment", "${var.environment}",
       "Project", "${var.project}"))
   }"
