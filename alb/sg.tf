f7dd988f7207acd2c5061431f693131fc27c3825
Luca Venturelli <luca@skyscrapers.eu>
2019-06-07 10:39:23 -0500

    ...

--- alb/sg.tf
+++ alb/sg.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "sg_alb" {
-  name_prefix = var.name_prefix
-  name        = var.name_prefix == null ? "${var.project}-${var.environment}-${var.name}-sg_alb" : null
+  name_prefix = var.name == null ? var.name_prefix : null
+  name        = var.name != null ? "${var.project}-${var.environment}-${var.name}-sg_alb" : null
   description = "Security group for ALB ${var.project}-${var.environment}-${local.name}-alb"
   vpc_id      = var.vpc_id
 
