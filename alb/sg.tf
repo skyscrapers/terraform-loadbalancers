028a3866146d7afe6cf43f26062a9c63c2a7a672
Luca Venturelli <luca@skyscrapers.eu>
2019-06-07 09:49:59 -0500

    fix sg description

--- alb/sg.tf
+++ alb/sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "sg_alb" {
   name_prefix = var.name_prefix
   name        = var.name_prefix == null ? "${var.project}-${var.environment}-${var.name}-sg_alb" : null
-  description = "Security group for ALB ${var.project}-${var.environment}-${local.name}-sg_alb"
+  description = "Security group for ALB ${var.project}-${var.environment}-${local.name}-alb"
   vpc_id      = var.vpc_id
 
   tags = merge(
